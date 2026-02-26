751fc48a6ff0517f878e8f7937b3660aa752d367
srujal <srujaldoshi05@gmail.com>
2026-02-27 02:22:25 +0530

    Update home URL path to remove protocol

--- view/url.py
+++ view/url.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('https://srujaldoshi05-ctrl.github.io/python/', home, name='home'),
+    path('srujaldoshi05-ctrl.github.io/python', home, name='home'),
     path('save-score/', save_score, name='save_score'),
     path('get-scores/', get_scores, name='get_scores'),
     path('get-leaderboard/', get_leaderboard, name='get_leaderboard'),
